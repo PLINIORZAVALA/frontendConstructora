71cbb68029aa95cde8aa6fedab462070e8174479
PLINIORZAVALA <134644204+PLINIORZAVALA@users.noreply.github.com>
2024-11-15 00:23:30 -0500

    Añadimos mas campos a catalogo-interfase.ts y tambien el el html y dimos siseño en la tabla que capturamos todos los datos del html con CSS del mismo modulo

--- src/app/interfaces/create-catalogo.interface.ts
+++ src/app/interfaces/create-catalogo.interface.ts
@@ -1,7 +1,9 @@
 export interface Catalogo {
     id: number;
     nombre: string;
+    tipo: string;
     descripcion: string;
+    imagen: string;
     // Otros campos según la estructura del catálogo
   }
   
