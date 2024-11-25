94ac94c1632db783ae0f03a4ba605afb14a9ffda
PLINIORZAVALA <134644204+PLINIORZAVALA@users.noreply.github.com>
2024-11-25 00:50:00 -0500

    Creacion de componentes similares al html de la plantilla

---
+++ src/app/shared/Admin/Zcomponens/book-marks/book-marks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-marks',
+  standalone: true,
+  imports: [],
+  templateUrl: './book-marks.component.html',
+  styleUrl: './book-marks.component.css'
+})
+export class BookMarksComponent {
+
+}
